5ae62c8c2a4d42f4c0a72c67f9c23ca42634377f
Show Wai Yan <showwaiyan555@gmail.com>
2025-05-10 13:14:56 +0800

    Finish 5.2P

---
+++ PassTask/5.2P_Iteration3/SwinAdventure/GameObject.cs
@@ -0,0 +1,38 @@
+namespace SwinAdventure
+{
+    public abstract class GameObject : IndentifiableObject
+    {
+        // Fields
+        private string _description;
+        private string _name;
+
+        // Constructor
+        public GameObject(string[] ids, string name, string desc) : base(ids)
+        {
+            _name = name;
+            _description = desc;
+        }
+
+        // Properties
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public string ShortDescription
+        {
+            get
+            {
+                char firstChar = char.ToLower(Name[0]);
+                string article = (firstChar == 'a' || firstChar == 'e' || firstChar == 'i' ||
+                                 firstChar == 'o' || firstChar == 'u') ? "an" : "a";
+                return $"{article} {Name.ToLower()} ({FirstId})";
+            }
+        }
+
+        public virtual string FullDescription
+        {
+            get { return _description; }
+        }
+    }
+}
