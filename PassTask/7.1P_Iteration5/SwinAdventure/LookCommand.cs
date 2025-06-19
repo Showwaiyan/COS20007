d76375b5057b7331aa84faf60527a7e481a027cc
Show Wai Yan <showwaiyan555@gmail.com>
2025-06-19 10:16:32 +0800

    Fix inventory/inv id

--- PassTask/7.1P_Iteration5/SwinAdventure/LookCommand.cs
+++ PassTask/7.1P_Iteration5/SwinAdventure/LookCommand.cs
@@ -3,50 +3,63 @@ namespace SwinAdventure
     public class LookCommand : Command
     {
         public LookCommand()
-            : base(new string[] { "look" }) { }
+            : base(new string[] { "look", "inventory", "inv" }) { }
 
         public override string Execute(Player p, string[] text)
         {
-            IHaveInventory? container = null;
-            string containerId;
-            string itemId;
-
-            if (text[0].ToLower() != "look")
-                return "Error in look input";
-
+            IHaveInventory container = null;
+            string containerId = null;
+            string itemId = null;
             switch (text.Length)
             {
-                case 3:
+                case 1: // inventory case
+                    if (text[0].ToLower() == "inventory" || text[0].ToLower() == "inv")
+                    {
+                        container = p;
+                        itemId = "me";
+                        break;
+                    }
+                    else return "Error in look input";
+                case 3: // look at <something>
+                    if (text[0].ToLower() != "look")
+                        return "Error in look input";
+
                     if (text[1].ToLower() != "at")
                         return "What do you want to look at?";
+
                     container = p;
+                    itemId = text[2].ToLower();
                     break;
-                case 5:
+                case 5: // look at <something> in <something>
+                    if (text[0].ToLower() != "look")
+                        return "Error in look input";
+
                     if (text[3].ToLower() != "in")
                         return "What do you want to look in?";
+
                     containerId = text[4].ToLower();
                     container = FetchContainer(p, containerId);
+                    itemId = text[2].ToLower();
                     break;
                 default:
                     return "I don\'t know how to look like that";
             }
 
-            itemId = text[2].ToLower();
             return LookAtIn(itemId, container);
         }
 
-        private IHaveInventory? FetchContainer(Player p, string containerId)
+        private IHaveInventory FetchContainer(Player p, string containerId)
         {
             return p.Locate(containerId) as IHaveInventory;
         }
 
-        private string LookAtIn(string thingId, IHaveInventory? container)
+        private string LookAtIn(string thingId, IHaveInventory container)
         {
             if (container == null)
                 return "I cannot find the bag";
 
             if (container.Locate(thingId) == null)
-                return $"I cannot find the {thingId} in the bag";
+                return $"I cannot find the {thingId} in the {container.Name}";
 
             return container.Locate(thingId).FullDescription;
         }
